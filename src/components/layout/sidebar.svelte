1fb8b94002bb871d39779d56cba8a55cf16bad2e
Miika Oinonen <mhtoin@utu.fi>
2025-04-17 18:51:47 +0300

    Sidebar adjustments

--- src/components/layout/sidebar.svelte
+++ src/components/layout/sidebar.svelte
@@ -1,14 +1,27 @@
-<div class="sidebar" box-="square"></div>
+<div class="sidebar-container">
+    <div class="sidebar" box-="square contain:!top">
+        <div><span is-="badge" variant-="pink">Documentation</span></div>
+    </div>
+</div>
 
 <style>
-    .sidebar {
+    .sidebar-container {
         height: 100%;
         width: 100%;
+        max-width: 30ch;
+    }
+
+    .sidebar {
+        display: flex;
+        flex-direction: column;
+        gap: 1lh;
+        overflow: hidden;
+        flex-grow: 1;
         background-color: var(--background-color);
         --box-border-color: var(--background1);
+        height: 95%;
         margin-left: 2ch;
         margin-right: 2ch;
         margin-top: 2ch;
-        margin-bottom: 2ch;
     }
 </style>
