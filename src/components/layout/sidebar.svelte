97612385afb24eaedac9de3ebae486cf302ea55c
Miika Oinonen <mhtoin@utu.fi>
2025-04-17 16:25:38 +0300

    Add layout and a skeleton of a sidebar

---
+++ src/components/layout/sidebar.svelte
@@ -0,0 +1,14 @@
+<div class="sidebar" box-="square"></div>
+
+<style>
+    .sidebar {
+        height: 100%;
+        width: 100%;
+        background-color: var(--background-color);
+        --box-border-color: var(--background1);
+        margin-left: 2ch;
+        margin-right: 2ch;
+        margin-top: 2ch;
+        margin-bottom: 2ch;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,28 @@
 const { children } = $props();
 import "../styles/global.css";
 import "../styles/typography.css";
+import Sidebar from "../components/layout/sidebar.svelte";
 </script>
 
-{@render children()}
+<div class="layout">
+    <Sidebar />
+    <div class="content">
+        {@render children()}
+    </div>
+</div>
+
+<style>
+    .layout {
+        display: grid;
+        gap: 10ch;
+        grid-template-columns: 1fr 3fr;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .content {
+        padding: 2rem;
+    }
+</style>
+
